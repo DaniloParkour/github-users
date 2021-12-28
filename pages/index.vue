51df52e67da6962a29d437c0ad0002870f98d170
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-12-28 15:52:37 -0300

    Tabs with rounded 2 vertices visual

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      Home
-    </v-col>
-  </v-row>
+  <v-container fluid>
+    <v-row justify="left" align="center">
+      <v-col cols="3">
+        <v-card class="mb-2 pa-1 twoRoundedVertices" color="#CC7788">
+          <v-toolbar color="#DF89A8" dark flat>
+            <v-tabs align-with-title>
+              <v-tabs-slider color="white"></v-tabs-slider>
+              <v-tab>
+                <v-btn dark icon>
+                  <v-icon>mdi-home</v-icon>
+                </v-btn>
+              </v-tab>
+              <v-tab>
+                <v-btn dark icon>
+                  <v-badge content="2" color="orange">
+                    <v-icon>mdi-cart</v-icon>
+                  </v-badge>
+                </v-btn>
+              </v-tab>
+            </v-tabs>
+          </v-toolbar>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -11,3 +31,12 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style scoped>
+.twoRoundedVertices {
+  border-radius: 50px 0px 50px 0px !important;
+}
+.border {
+  border: 2px solid white !important;
+}
+</style>

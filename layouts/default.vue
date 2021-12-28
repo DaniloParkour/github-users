e04677a61a42bfa1acf7f5eb0915d9c19cedee5e
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-12-28 15:35:17 -0300

    nuxt on layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app dark>
     <v-main>
-      Main Content
+      Template Main Content
+      <nuxt />
     </v-main>
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
@@ -14,24 +15,7 @@ export default {
   name: 'DefaultLayout',
   data() {
     return {
-      clipped: false,
-      drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
       title: 'Vuetify.js',
     }
   },

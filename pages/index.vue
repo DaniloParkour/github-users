4c2b0f562bbbc77c4bc730d08867ed5363e142da
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-12-28 16:50:23 -0300

    Card with informations

--- pages/index.vue
+++ pages/index.vue
@@ -26,6 +26,53 @@
          <v-text-field label="Search" prepend-inner-icon="mdi-search" filled rounded color="#DF89A8"></v-text-field>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col cols="6">
+        <v-card class="rounded-xl mt-2">
+            <v-row>
+              <v-col cols="3" align="left">
+                <v-avatar size="120" tile color="#AA88AA" class="rounded-circle ml-2" />
+              </v-col>
+              <v-col cols="9" class="px-2">
+                <h3 class="grey--text">Name Name</h3>
+                <p class="grey--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis consequuntur dolorum, 
+                      quibusdam eum pariatur, laboriosam quia provident placeat corrupti repellat odio at.
+                       Hic commodi .
+                </p>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="2">
+                <v-avatar color="#151515" size="30" class="ml-2">
+                  <v-icon small color="pink">mdi-calendar</v-icon>
+                </v-avatar>
+                200
+              </v-col>
+              <v-col cols="2">
+                <v-avatar color="#151515" size="30" class="ml-2">
+                  <v-icon small color="pink">mdi-star</v-icon>
+                </v-avatar>
+                80
+              </v-col>
+              <v-col cols="2">
+                <v-avatar color="#151515" size="30" class="ml-2">
+                  <v-icon small color="pink">mdi-heart</v-icon>
+                </v-avatar>
+                20
+              </v-col>
+              <v-col cols="2">
+                <v-avatar color="#151515" size="30" class="ml-2">
+                  <v-icon small color="pink">mdi-file</v-icon>
+                </v-avatar>
+                45%
+              </v-col>
+              <v-col cols="4" align="end">
+                <v-btn rounded color="pink" class="mr-2"><v-icon left>mdi-plus</v-icon>Add to cart</v-btn>
+              </v-col>
+            </v-row>
+          </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
